a4c3ab335765434209d9284c7d67d86a864dbe82
Noel Eugenio <noel.shirodev@gmail.com>
2016-10-30 14:16:00 -0400

    201610301415:
    Added findnode
    Added getindex
    Added addadj
    Added logic for each function

--- Assets/Scripts/Search.cs
+++ Assets/Scripts/Search.cs
@@ -66,5 +66,77 @@ public class Search
         }
 
         iter++;
+
+        //Pick a node
+        var node = ChooseNode();
+        //Check if the node we picked is the goal node
+        if(node == endNode)
+        {
+            //Go back over our nodes to find the path
+            while(node != null)
+            {
+                path.Insert(0, node);
+                node = node.prev;
+            }
+
+            //Exit
+            isDone = true;
+            return;
+        }
+
+        //Remove the node from the list
+        possible.Remove(node);
+        //Add it to our visited list
+        visited.Add(node);
+
+        //Iterate through adj nodes
+        for(var i = 0; i < node.adj.Count; i++)
+        {
+            addAdj(node, node.adj[i]);
+        }
+    }
+
+    //Populate solution
+    public void addAdj(Node node, Node adj)
+    {
+        if(findNode(adj, visited) || findNode(adj, possible))
+        {
+            return;
+        }
+
+        //Add current node to adj node previouse field
+        adj.prev = node;
+        //Add to reachable list
+        possible.Add(adj);
+    }
+
+    //Finding the next node
+    public bool findNode(Node node, List<Node> list)
+    {
+        return getIndex(node, list) >= 0;
+    }
+
+    //Get the index of a node
+    public int getIndex(Node node, List<Node> list)
+    {
+        //Check if node exists in the list
+        for(var i = 0; i < list.Count; i++)
+        {
+            //Check if the node is equal to a node in the list
+            if(node == list[i])
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    //Pick a node
+    //Normally, nodes will be weighted so we can choose the best possible path
+    //In this case, we will grab a random node from the list
+    public Node ChooseNode()
+    {
+        return possible[Random.Range(0, possible.Count)];
     }
 }
