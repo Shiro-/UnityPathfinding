5d80b235823e872f2a8b4e0bb2103ba6433e1137
Noel Eugenio <noel.shirodev@gmail.com>
2016-10-29 20:39:27 -0400

    201610292039:
    Added list to hold the nodes

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -6,5 +6,15 @@ using System.Collections.Generic;
 
 public class Node
 {
+    //List for the nodes
+    public List<Node> adj = new List<Node>();
+    public Node prev = null;
+    //Name for nodes
+    public string lbl = "";
 
+    //Clear the node
+    public void Clear()
+    {
+        prev = null;
+    }
 }
