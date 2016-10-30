a0005dcaaba77814c0633ac22d14d54d8116510b
Noel Eugenio <noel.shirodev@gmail.com>
2016-10-29 21:57:03 -0400

    201610292156:
    Added lists to keep track of our nodes while search the graph

---
+++ Assets/Scripts/Search.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Search
+{
+    //Reference to graph
+    public Graph graph;
+    //Keep track of our nodes
+    public List<Node> a;
+    public List<Node> b;
+    public List<Node> c;
+    public Node endNode;
+    //Keep track of our iterations
+    public int iter;
+    //Check if the search is done
+    public bool isDone;
+}
