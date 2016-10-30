b69f909016c577e8d874e3a1688b2de299eff155
Noel Eugenio <noel.shirodev@gmail.com>
2016-10-29 20:22:00 -0400

    201610292021:
    Added node script

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,10 @@
+﻿//The node will represent a place on the map/grid
+//This is the most important part of the algorithm
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Node
+{
+
+}
